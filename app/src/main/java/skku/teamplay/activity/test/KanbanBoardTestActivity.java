4b3fc31cd5b1b13274f2a6bd8f899dfeed4abf52
jinho <ddjddd@naver.com>
2018-05-15 18:21:03 +0900

    Viewpager + firebase

--- app/src/main/java/skku/teamplay/activity/test/KanbanBoardTestActivity.java
+++ app/src/main/java/skku/teamplay/activity/test/KanbanBoardTestActivity.java
@@ -2,6 +2,7 @@ package skku.teamplay.activity.test;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import skku.teamplay.R;
 import skku.teamplay.activity.adapter.KanbanFragmentAdapter;
 import skku.teamplay.fragment.test.KanbanFragment;
@@ -18,31 +20,27 @@ import skku.teamplay.fragment.test.KanbanFragment;
 
 public class KanbanBoardTestActivity extends FragmentActivity {
     @BindView(R.id.pager) ViewPager pager;
+    @BindView(R.id.addFAB) FloatingActionButton addFAB;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.kanban_viewpager);
+        setContentView(R.layout.kanban_viewpager_test);
         ButterKnife.bind(this);
         PagerAdapter adapter = new KanbanFragmentAdapter(getSupportFragmentManager());
         pager.setAdapter(adapter);
     }
 
+//    @OnClick(R.id.addFAB)
+//    void onAddFabClick (R.id.)
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
         // 여기서 프래그먼트 리시버를 아예 오버라이드 하는 방법을 생각 중입니다.
         super.onActivityResult(requestCode, resultCode, data);
-        int request = requestCode & 0Xffff;
-//        KanbanFragment fragment = (KanbanFragment) getSupportFragmentManager().findFragmentById(0);
-//        KanbanFragment fragment = pager.getCurrentItem();
-        Fragment fragment = getSupportFragmentManager().findFragmentById(0);
-
-
-        Toast.makeText(getApplicationContext(), Integer.toString(i), Toast.LENGTH_LONG).show();
-//        fragment.onResult(requestCode, resultCode, data);
-//        fragment.onActivityResult(request, resultCode, data);
-//    getSupportFragmentManager().getBackStackEntryAt(getSupportFragmentManager().getBackStackEntryCount() - 1).getId()
+        for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+            fragment.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
 
--- app/src/main/java/skku/teamplay/fragment/test/KanbanFragment.java
+++ app/src/main/java/skku/teamplay/fragment/test/KanbanFragment.java
@@ -2,22 +2,29 @@ package skku.teamplay.fragment.test;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import butterknife.BindView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
 import skku.teamplay.R;
 import skku.teamplay.activity.adapter.KanbanQuestlistAdapter;
 import skku.teamplay.activity.dialog.QuestPopupDialog;
 import skku.teamplay.models.Quest;
+
+import static android.content.ContentValues.TAG;
+
 
 /**
  * Created by ddjdd on 2018-05-11.
@@ -27,6 +34,8 @@ public class KanbanFragment extends Fragment {
 
     private int mPageNumber;
 
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
 
     public static KanbanFragment create(int pageNumber) {
         KanbanFragment fragment = new KanbanFragment();
@@ -49,7 +58,7 @@ public class KanbanFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View fragmentLayout = inflater.inflate(R.layout.kanban_test, container, false);
-        QuestList = (ListView) fragmentLayout.findViewById(R.id.quest_list);;
+        QuestList = (ListView) fragmentLayout.findViewById(R.id.quest_list);
         adapter =  new KanbanQuestlistAdapter();
 
         showQuestList();
@@ -75,78 +84,68 @@ public class KanbanFragment extends Fragment {
         return fragmentLayout;
     }
 
-    public void onResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        int request = requestCode & 0Xffff;
-//        KanbanFragment fragment = (KanbanFragment) getSupportFragmentManager().findFragmentById(0);
-//        fragment.onResult(requestCode, resultCode, data);
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String title, description, startAt, dueAt;
+        int type, reward, pos;
 
-//    getSupportFragmentManager().getBackStackEntryAt(getSupportFragmentManager().getBackStackEntryCount() - 1).getId()
-    }
-
-
-//    public void onResult (int requestCode, int resultCode, Intent data) {
-//
-//        String title, description, startAt, dueAt;
-//        int type, reward, pos;
-//
-//        if (requestCode == 1) {
-//            switch (resultCode) {
-//                case 10:        // 제거
+        if (requestCode == 1) {
+            switch (resultCode) {
+                case 10:        // 제거
+                    pos = data.getIntExtra("pos", -1);
+                    if (pos != -1) {
+                        adapter.deleteItem(pos);
+                        adapter.notifyDataSetChanged();
+                    }
+                    break;
+                case 100:       // 완료
 //                    pos = data.getIntExtra("pos", -1);
 //                    if (pos != -1) {
-////                        adapter.deleteItem(pos);
-////                        adapter.notifyDataSetChanged();
+//                        Quest finishedQuest = (Quest) adapter.getItem(pos);
+//                        adapter.makeFinish(pos, finishedQuest);
+//                        adapter.notifyDataSetChanged();
 //                    }
-//                    break;
+                    break;
+
+                case 1000:      // 추가
+//                    title = data.getStringExtra("title");
+//                    description = data.getStringExtra("description");
+//                    startAt = data.getStringExtra("startAt");
+//                    dueAt = data.getStringExtra("dueAt");
+//                    type = Integer.parseInt(data.getStringExtra("type"));
+//                    reward = Integer.parseInt(data.getStringExtra("reward"));
+//                    Quest newQuest = new Quest(title, description, false, startAt, dueAt, "1111", type, reward);
 //
-////                case 100:       // 완료
-////                    pos = data.getIntExtra("pos", -1);
-////                    if (pos != -1) {
-////                        Quest finishedQuest = (Quest) adapter.getItem(pos);
-////                        adapter.makeFinish(pos, finishedQuest);
-////                        adapter.notifyDataSetChanged();
-////                    }
-////                    break;
-////
-////                case 1000:      // 추가
-////                    title = data.getStringExtra("title");
-////                    description = data.getStringExtra("description");
-////                    startAt = data.getStringExtra("startAt");
-////                    dueAt = data.getStringExtra("dueAt");
-////                    type = Integer.parseInt(data.getStringExtra("type"));
-////                    reward = Integer.parseInt(data.getStringExtra("reward"));
-////                    Quest newQuest = new Quest(title, description, false, startAt, dueAt, "1111", type, reward);
-////
-////                    adapter.addItem(newQuest);
-////                    adapter.notifyDataSetChanged();
-////                    break;
-////
-////                case 2000:      // 변경
-////                    title = data.getStringExtra("title");
-////                    description = data.getStringExtra("description");
-////                    startAt = data.getStringExtra("startAt");
-////                    dueAt = data.getStringExtra("dueAt");
-////                    type = Integer.parseInt(data.getStringExtra("type"));
-////                    reward = Integer.parseInt(data.getStringExtra("reward"));
-////
-////                    pos = data.getIntExtra("pos", -1);
-////                    if (pos != -1) {
-////                        Quest modifiedQuest = (Quest) adapter.getItem(pos);
-////                        modifiedQuest.setTitle(title);
-////                        modifiedQuest.setDescription(description);
-////                        modifiedQuest.setStartAt(startAt);
-////                        modifiedQuest.setDueAt(dueAt);
-////                        modifiedQuest.setRewardType(type);
-////                        modifiedQuest.setReward(reward);
-////                        adapter.modifyItem(pos, modifiedQuest);
-////                        adapter.notifyDataSetChanged();
-////                    }
-////
-////                    break;
-//            }
-//        }
-//    }
+//                    adapter.addItem(newQuest);
+//                    adapter.notifyDataSetChanged();
+                    break;
+
+                case 2000:      // 변경
+//                    title = data.getStringExtra("title");
+//                    description = data.getStringExtra("description");
+//                    startAt = data.getStringExtra("startAt");
+//                    dueAt = data.getStringExtra("dueAt");
+//                    type = Integer.parseInt(data.getStringExtra("type"));
+//                    reward = Integer.parseInt(data.getStringExtra("reward"));
+//
+//                    pos = data.getIntExtra("pos", -1);
+//                    if (pos != -1) {
+//                        Quest modifiedQuest = (Quest) adapter.getItem(pos);
+//                        modifiedQuest.setTitle(title);
+//                        modifiedQuest.setDescription(description);
+//                        modifiedQuest.setStartAt(startAt);
+//                        modifiedQuest.setDueAt(dueAt);
+//                        modifiedQuest.setRewardType(type);
+//                        modifiedQuest.setReward(reward);
+//                        adapter.modifyItem(pos, modifiedQuest);
+//                        adapter.notifyDataSetChanged();
+//                    }
+
+                    break;
+            }
+        }
+    }
 
     private void showQuestList() {
         QuestList.setAdapter(adapter);
@@ -156,6 +155,34 @@ public class KanbanFragment extends Fragment {
         Quest example2 = new Quest("커피사기", "2잔사기", false, "18.02.03", "18.02.03", "jinho", 1, 10);
         adapter.addItem(example);
         adapter.addItem(example2);
+
+        databaseReference.child("quest").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                Quest newQuest = dataSnapshot.getValue(Quest.class);
+                Log.e("LOG", "dataSnapshot.getKey() : " + dataSnapshot.getKey());
+//                adapter.addItem(dataSnapshot.getKey());
+            }
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+        });
+
 
     }
 }
