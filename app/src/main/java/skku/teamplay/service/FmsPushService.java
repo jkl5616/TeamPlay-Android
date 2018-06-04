cb3b6c85377ba18d505355bdf7a890f3ea4d955c
jkl5616 <jkl5616@gmail.com>
2018-06-04 10:44:47 +0900

    Update FmsPushService

--- app/src/main/java/skku/teamplay/service/FmsPushService.java
+++ app/src/main/java/skku/teamplay/service/FmsPushService.java
@@ -1,15 +1,43 @@
 package skku.teamplay.service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceIdService;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import skku.teamplay.R;
+import skku.teamplay.activity.MainActivity;
+
 public class FmsPushService extends FirebaseMessagingService {
 
+    private int id = 0;
+
     @Override
-    public void onMessageReceived(RemoteMessage rm) {
-        Log.e("TeamPlay", "new firebasepush message");
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        String title = remoteMessage.getData().get("title");
+        String content = remoteMessage.getData().get("content");
+        showNoti(title, content);
     }
+
+    private void showNoti(String title, String content) {
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1,
+                new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .build();
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.notify(id++, notification);
+    }
+
+
 }
