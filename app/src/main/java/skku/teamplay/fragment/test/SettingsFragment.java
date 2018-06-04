5a71780387bc941ddd214dc3d32a52a52092cc59
Woolim Cho <jkl5616@gmail.com>
2018-06-05 07:25:33 +0900

    Update SettingsFragment

--- app/src/main/java/skku/teamplay/fragment/test/SettingsFragment.java
+++ app/src/main/java/skku/teamplay/fragment/test/SettingsFragment.java
@@ -1,6 +1,13 @@
 package skku.teamplay.fragment.test;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import skku.teamplay.R;
 
 /**
  * Created by woorim on 2018. 6. 4..
@@ -9,4 +16,21 @@ import android.support.v4.app.Fragment;
 
 //팀원 추가, 데드라인 설정, 규칙 추가, 페널티 등 팀 설정
 public class SettingsFragment extends Fragment {
+
+    View rootView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (rootView != null) return rootView;
+        rootView = (ViewGroup)inflater.inflate(R.layout.fragment_settings, container, false);
+
+        return rootView;
+    }
+
 }
