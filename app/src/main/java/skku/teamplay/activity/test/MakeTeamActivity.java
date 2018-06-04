64a44605b81184e2efa7e95d5abaa616b9de34f4
jkl5616 <jkl5616@gmail.com>
2018-06-04 10:08:21 +0900

    Add MakeTeamActivity

--- app/src/main/java/skku/teamplay/activity/test/MakeTeamActivity.java
+++ app/src/main/java/skku/teamplay/activity/test/MakeTeamActivity.java
@@ -1,8 +1,18 @@
 package skku.teamplay.activity.test;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import skku.teamplay.R;
+
 /**
  * Created by woorim on 2018. 6. 4..
  */
 
-public class MakeTeamActivity {
+public class MakeTeamActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_team);
+    }
 }
