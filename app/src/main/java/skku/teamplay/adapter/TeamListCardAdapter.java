6b35859dd27f55f30ef92d7465cf79fb9aeb4b68
JunKyus <34362701+kuk941025@users.noreply.github.com>
2018-06-05 11:29:28 +0900

    countdowntimer added

--- app/src/main/java/skku/teamplay/adapter/TeamListCardAdapter.java
+++ app/src/main/java/skku/teamplay/adapter/TeamListCardAdapter.java
@@ -1,5 +1,6 @@
 package skku.teamplay.adapter;
 
+import android.os.CountDownTimer;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,10 +15,10 @@ import butterknife.ButterKnife;
 import skku.teamplay.R;
 import skku.teamplay.app.TeamPlayApp;
 import skku.teamplay.model.Team;
+import skku.teamplay.widget.CircleCountdownView;
 
 public class TeamListCardAdapter extends BaseAdapter{
     ArrayList<Team> teamList;
-
     public TeamListCardAdapter(ArrayList<Team> teamList) {
         this.teamList = teamList;
     }
@@ -48,6 +49,7 @@ public class TeamListCardAdapter extends BaseAdapter{
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         ViewHolder holder = null;
+        long[] time = new long[4];
         if(view == null) {
             view = LayoutInflater.from(TeamPlayApp.getAppInstance()).inflate(R.layout.list_team_template_card, null);
             holder = new ViewHolder(view);
@@ -56,14 +58,27 @@ public class TeamListCardAdapter extends BaseAdapter{
             holder = (ViewHolder) view.getTag();
         }
         holder.name.setText(getItem(i).getName());
+
+        time[0] = 1;
+        time[1] = 2;
+        time[2] = 3;
+        time[3] = 4;
+        CountDownAdapter countDownAdapter = new CountDownAdapter(holder.circleTimeView, time);
+        countDownAdapter.start();
         return view;
     }
 
     public class ViewHolder {
+        CircleCountdownView[] circleTimeView = new CircleCountdownView[4];
         @BindView(R.id.tv_teamitem_teamname) TextView name;
 
         public ViewHolder(View view) {
             ButterKnife.bind(this, view);
+            circleTimeView[0] = view.findViewById(R.id.template_circle_day);
+            circleTimeView[1] = view.findViewById(R.id.template_circle_hour);
+            circleTimeView[2] = view.findViewById(R.id.template_circle_min);
+            circleTimeView[3] = view.findViewById(R.id.template_circle_secs);
+
         }
     }
 
