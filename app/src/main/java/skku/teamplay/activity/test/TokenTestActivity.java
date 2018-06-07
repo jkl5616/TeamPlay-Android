8575beca4afe6bc9199a59cda89a43276e888648
Woolim Cho <jkl5616@gmail.com>
2018-06-07 21:28:03 +0900

    Add TokenTestActivity2

--- app/src/main/java/skku/teamplay/activity/test/TokenTestActivity.java
+++ app/src/main/java/skku/teamplay/activity/test/TokenTestActivity.java
@@ -1,4 +1,17 @@
 package skku.teamplay.activity.test;
 
-public class TokenTestActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class TokenTestActivity extends Activity {
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        TextView tv = new TextView(this);
+        tv.setText(FirebaseInstanceId.getInstance().getToken());
+        setContentView(tv);
+    }
 }
