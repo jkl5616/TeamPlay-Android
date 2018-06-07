d061e580255503bb4606118e94be2ec424b4d46f
Woolim Cho <jkl5616@gmail.com>
2018-06-07 21:28:43 +0900

    Add missing apis

--- app/src/main/java/skku/teamplay/api/impl/AddAppointment.java
+++ app/src/main/java/skku/teamplay/api/impl/AddAppointment.java
@@ -1,4 +1,59 @@
 package skku.teamplay.api.impl;
 
-public class AddAppointment {
+import java.util.Date;
+
+import skku.teamplay.api.GenericResult;
+import skku.teamplay.api.RestApi;
+import skku.teamplay.api.RestApiResult;
+
+public class AddAppointment extends RestApi{
+
+    private int team_id;
+    private Date startDate;
+    private Date endDate;
+    private String description;
+
+    public AddAppointment(int team_id, Date startDate, Date endDate, String description) {
+        this.team_id = team_id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.description = description;
+    }
+
+    public int getTeam_id() {
+        return team_id;
+    }
+
+    public void setTeam_id(int team_id) {
+        this.team_id = team_id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public Class<? extends RestApiResult> getResultClass() {
+        return GenericResult.class;
+    }
 }
--- app/src/main/java/skku/teamplay/api/impl/GetAppointmentByTeam.java
+++ app/src/main/java/skku/teamplay/api/impl/GetAppointmentByTeam.java
@@ -1,4 +1,27 @@
 package skku.teamplay.api.impl;
 
-public class GetAppointmentByTeam {
+import skku.teamplay.api.RestApi;
+import skku.teamplay.api.RestApiResult;
+import skku.teamplay.api.impl.res.AppointmentListResult;
+
+public class GetAppointmentByTeam extends RestApi {
+    private int team_id;
+
+    public GetAppointmentByTeam(int team_id) {
+        this.team_id = team_id;
+    }
+
+
+    @Override
+    public Class<? extends RestApiResult> getResultClass() {
+        return AppointmentListResult.class;
+    }
+
+    public int getTeam_id() {
+        return team_id;
+    }
+
+    public void setTeam_id(int team_id) {
+        this.team_id = team_id;
+    }
 }
