7a73eede44731decebac3153861ce399bb57e4cc
jkl5616 <jkl5616@gmail.com>
2018-06-08 12:42:17 +0900

    fix colors

--- app/src/main/java/skku/teamplay/util/Util.java
+++ app/src/main/java/skku/teamplay/util/Util.java
@@ -86,13 +86,24 @@ public class Util {
     }
 
     private static void initGrayColors() {
-        String cols[] = {"#4c2620","#1f3a34","#143645","#D187FE","#291b32","#4573E7","#0d172e","#222132","#1d281f","#00111f"};
+        String cols[] = {"#FE816D","#68C4AF","#45B4E7","#D187FE","#ffb331","#4573E7","#6AECF4","#ADA7FC","#95CB9C","#01579B"};
         ArrayList<Integer> colArray = new ArrayList<Integer>();
         String alpha = "80";
         for(int i = 0; i < cols.length; i++) {
-            colArray.add(Color.parseColor("#"+alpha+cols[i].split("#")[1]));
+            colArray.add(manipulateColor(Color.parseColor("#"+alpha+cols[i].split("#")[1]), 0.3f));
         }
         graycolors = colArray.toArray(new Integer[colArray.size()]);
+    }
+
+    public static int manipulateColor(int color, float factor) {
+        int a = Color.alpha(color);
+        int r = Math.round(Color.red(color) * factor);
+        int g = Math.round(Color.green(color) * factor);
+        int b = Math.round(Color.blue(color) * factor);
+        return Color.argb(a,
+                Math.min(r,255),
+                Math.min(g,255),
+                Math.min(b,255));
     }
 
     public static int nextGrayColor() {
