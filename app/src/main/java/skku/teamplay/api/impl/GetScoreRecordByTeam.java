c7da22f2ca43b47e071349a85f9db9d023c9f171
Woorim Cho <jkl5616@gmail.com>
2018-06-09 14:45:31 +0900

    fix GetScoreRecordByTeam

--- app/src/main/java/skku/teamplay/api/impl/GetScoreRecordByTeam.java
+++ app/src/main/java/skku/teamplay/api/impl/GetScoreRecordByTeam.java
@@ -5,6 +5,21 @@ import skku.teamplay.api.RestApiResult;
 import skku.teamplay.api.impl.res.ScoreRecordListResult;
 
 public class GetScoreRecordByTeam extends RestApi {
+
+    private int team_id;
+
+    public GetScoreRecordByTeam(int team_id) {
+        this.team_id = team_id;
+    }
+
+    public int getTeam_id() {
+        return team_id;
+    }
+
+    public void setTeam_id(int team_id) {
+        this.team_id = team_id;
+    }
+
     @Override
     public Class<? extends RestApiResult> getResultClass() {
         return ScoreRecordListResult.class;
