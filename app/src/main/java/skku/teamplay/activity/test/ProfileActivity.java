46b3e860501311ea4cadee140aecb09d4f5871ae
Woolim Cho <jkl5616@gmail.com>
2018-06-10 17:25:01 +0900

    Some fixes & add timetablemodifyactivity

--- app/src/main/java/skku/teamplay/activity/test/ProfileActivity.java
+++ app/src/main/java/skku/teamplay/activity/test/ProfileActivity.java
@@ -41,7 +41,6 @@ import skku.teamplay.util.RecyclerItemClickListener;
  */
 
 public class ProfileActivity extends AppCompatActivity implements OnRestApiListener {
-    boolean MEMBER_LOAD_FLAG;
     TeamListCardAdapter teamAdapter;
     @BindView(R.id.layout_update_timetable)
     LinearLayout layout_update_timetable;
@@ -52,8 +51,12 @@ public class ProfileActivity extends AppCompatActivity implements OnRestApiListe
     @BindView(R.id.lv_team_list)
     ListView lv_teamList;
 
+    @BindView(R.id.tv_modify_timetable)
+    TextView tv_modify_timetable;
+
     @BindView(R.id.fab_main_profile)
     FabSpeedDial fab;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,12 +70,19 @@ public class ProfileActivity extends AppCompatActivity implements OnRestApiListe
             }
         });
 
+        tv_modify_timetable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfileActivity.this, TimetableModifyActivity.class));
+            }
+        });
+
         tv_profile.setText(TeamPlayApp.getAppInstance().getUser().getName());
-        fab.setMenuListener(new SimpleMenuListenerAdapter(){
+        fab.setMenuListener(new SimpleMenuListenerAdapter() {
             @Override
             public boolean onMenuItemSelected(MenuItem menuItem) {
                 int temp = 0;
-                switch (menuItem.getItemId()){
+                switch (menuItem.getItemId()) {
                     case R.id.menu1:
                         Intent intent = new Intent(ProfileActivity.this, MakeTeamActivity.class);
                         startActivity(intent);
@@ -101,37 +111,23 @@ public class ProfileActivity extends AppCompatActivity implements OnRestApiListe
                 TeamListResult result = (TeamListResult) restApiResult;
                 final ArrayList<Team> teamList = result.getTeamList();
                 teamAdapter = new TeamListCardAdapter(teamList);
-                        lv_teamList.setAdapter(teamAdapter);
-                        lv_teamList.hasFocus();
-                        lv_teamList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                            @Override
-                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                                Team team = teamList.get(i);
-                                TeamPlayApp.getAppInstance().setTeam(team);
-                                new RestApiTask(ProfileActivity.this).execute(new GetAllUsersByTeam(team.getId()));
-                                MEMBER_LOAD_FLAG = false;
+                lv_teamList.setAdapter(teamAdapter);
+                lv_teamList.hasFocus();
+                lv_teamList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Team team = teamList.get(i);
+                        TeamPlayApp.getAppInstance().setTeam(team);
+                        new RestApiTask(ProfileActivity.this).execute(new GetAllUsersByTeam(team.getId()));
                     }
                 });
-                MEMBER_LOAD_FLAG = true;
-                for (Team team : teamList){
-                    new RestApiTask(ProfileActivity.this).execute(new GetAllUsersByTeam(team.getId()));
-                }
-
                 break;
             case "getallusersbyteam":
                 UserListResult userListResult = (UserListResult) restApiResult;
-                if (MEMBER_LOAD_FLAG == false) {
-                    final ArrayList<User> userList = userListResult.getUserList();
-                    TeamPlayApp.getAppInstance().setUserList(userList);
-                    //탭 액티비티로 이동
-                    startActivity(new Intent(ProfileActivity.this, TabTestActivity.class));
-                }
-                else{ //load team members to a grid view
-
-                    ArrayList<User> tempList = userListResult.getUserList();
-                    teamAdapter.setContributorTitle("참여자 - 총 " + tempList.size() + "명", teamIdx);
-                    teamIdx++;
-                }
+                final ArrayList<User> userList = userListResult.getUserList();
+                TeamPlayApp.getAppInstance().setUserList(userList);
+                //탭 액티비티로 이동
+                startActivity(new Intent(ProfileActivity.this, TabTestActivity.class));
                 break;
         }
     }
--- app/src/main/java/skku/teamplay/service/FmsPushService.java
+++ app/src/main/java/skku/teamplay/service/FmsPushService.java
@@ -1,9 +1,14 @@
 package skku.teamplay.service;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -16,7 +21,7 @@ import skku.teamplay.activity.MainActivity;
 
 public class FmsPushService extends FirebaseMessagingService {
 
-    private int id = 0;
+    private int id = 1;
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -27,17 +32,28 @@ public class FmsPushService extends FirebaseMessagingService {
     }
 
     private void showNoti(String title, String content) {
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1,
-                new Intent(this, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
-        Notification notification = new NotificationCompat.Builder(this)
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "teamplay_channel";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            @SuppressLint("WrongConstant") NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "teamplay", NotificationManager.IMPORTANCE_MAX);
+            notificationChannel.setDescription("teamplay_push");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.GREEN);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_pac_icon)
+                .setTicker("TeamPlay")
                 .setContentTitle(title)
-                .setContentText(content)
-                .setContentIntent(pendingIntent)
-                .setSmallIcon(R.mipmap.ic_launcher_round)
-                .build();
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        manager.notify(id++, notification);
+                .setContentText(content);
+        notificationManager.notify(id++, notificationBuilder.build());
     }
-
-
 }
